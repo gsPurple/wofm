1eb9cddbc5682c5dd4fbf913bdeb1c8cb45dac4b
goncaloroxo <g.roxo@campus.fct.unl.pt>
2023-02-22 17:29:18 +0000

    Updates

---
+++ precache-manifest.b184fab512c280dd6166a36eed5b0ebd.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc7cd69f759ac417561c",
+    "url": "/wofm/static/css/main.23619fce.chunk.css"
+  },
+  {
+    "revision": "cc7cd69f759ac417561c",
+    "url": "/wofm/static/js/main.cc7cd69f.chunk.js"
+  },
+  {
+    "revision": "d3e7a9cb812acc5d1dba",
+    "url": "/wofm/static/js/1.d3e7a9cb.chunk.js"
+  },
+  {
+    "revision": "cf3f265da2852b9b5598",
+    "url": "/wofm/static/js/runtime~main.cf3f265d.js"
+  },
+  {
+    "revision": "ffaa817c296e9121150351872d13fe25",
+    "url": "/wofm/static/media/bareRoom.ffaa817c.png"
+  },
+  {
+    "revision": "484388009c70d9d24ac3b9ea06365593",
+    "url": "/wofm/static/media/decoratedRoom.48438800.png"
+  },
+  {
+    "revision": "19934cd28c3561bc0a5fa8a7b193ce4e",
+    "url": "/wofm/static/media/giantSpider.19934cd2.png"
+  },
+  {
+    "revision": "7b3108a32c852d830d683822a9cfeed9",
+    "url": "/wofm/static/media/giverSleep.7b3108a3.png"
+  },
+  {
+    "revision": "ba4b2fc2c42642d5386d42d56092a60e",
+    "url": "/wofm/static/media/oneEyeStatue.ba4b2fc2.png"
+  },
+  {
+    "revision": "292d50a274f1be4e22c032c001f0bef9",
+    "url": "/wofm/static/media/rats.292d50a2.jpg"
+  },
+  {
+    "revision": "7291d3457e1ee942c76c034266a3a53e",
+    "url": "/wofm/static/media/smallRoomKey.7291d345.png"
+  },
+  {
+    "revision": "13111de3799d7c98b300aeb35e82ecaf",
+    "url": "/wofm/static/media/store.13111de3.png"
+  },
+  {
+    "revision": "39de2c00b27ffe34221210c69a7558ef",
+    "url": "/wofm/static/media/troll.39de2c00.png"
+  },
+  {
+    "revision": "a236b910505f84c5faabdb22fcdcf89c",
+    "url": "/wofm/static/media/tubularworm.a236b910.png"
+  },
+  {
+    "revision": "d2fdf5b7f871574f5893493388ca7650",
+    "url": "/wofm/static/media/wizardAndGremlin.d2fdf5b7.png"
+  },
+  {
+    "revision": "248ec026f0b2664c76139a0d41e3ec7b",
+    "url": "/wofm/static/media/wofm001.248ec026.png"
+  },
+  {
+    "revision": "f115197f558fb8f55d7e37c078df7f3b",
+    "url": "/wofm/static/media/wofm002.f115197f.png"
+  },
+  {
+    "revision": "6710660733459bf2711cd89b4138c97e",
+    "url": "/wofm/static/media/wofm003.67106607.png"
+  },
+  {
+    "revision": "c3474090cfe1ff89831ca7c9efb3cd00",
+    "url": "/wofm/static/media/wofm004.c3474090.png"
+  },
+  {
+    "revision": "117421cee8db81f16d57427683566a19",
+    "url": "/wofm/static/media/wofm005.117421ce.png"
+  },
+  {
+    "revision": "ba01d585e5d5364087de5a9ac7210f53",
+    "url": "/wofm/static/media/wofm006.ba01d585.png"
+  },
+  {
+    "revision": "770ccb35cb729b0c605478c324f715d3",
+    "url": "/wofm/static/media/wofm007.770ccb35.png"
+  },
+  {
+    "revision": "67a1b1952e99e8e3bf6b987538f187f5",
+    "url": "/wofm/static/media/wofm134.67a1b195.png"
+  },
+  {
+    "revision": "3f7fe6b46a923f2fb40c1759156b7645",
+    "url": "/wofm/static/media/wofm155.3f7fe6b4.png"
+  },
+  {
+    "revision": "7ed3b1e87f8b7b78f59ecc6b83030270",
+    "url": "/wofm/static/media/wofm218.7ed3b1e8.png"
+  },
+  {
+    "revision": "cfbe4df01e70bc68742d208ded362317",
+    "url": "/wofm/static/media/wofm251.cfbe4df0.png"
+  },
+  {
+    "revision": "b19288bfd4a85cd45e1e12ec0d9d6351",
+    "url": "/wofm/static/media/wofm336.b19288bf.png"
+  },
+  {
+    "revision": "fadb873d11b785a752d6c6f292e633cc",
+    "url": "/wofm/static/media/wofm370.fadb873d.png"
+  },
+  {
+    "revision": "72edd2b6eefa5408383549bf8216f97e",
+    "url": "/wofm/static/media/wofm383.72edd2b6.png"
+  },
+  {
+    "revision": "e7874d55e762bf07da58ddac6d529d21",
+    "url": "/wofm/static/media/wofmsword.e7874d55.png"
+  },
+  {
+    "revision": "90492294488400d2c86b53760e9219b4",
+    "url": "/wofm/static/media/woodBench.90492294.png"
+  },
+  {
+    "revision": "b5a1ba34183ac450ddd28f5f1005d847",
+    "url": "/wofm/index.html"
+  }
+];
